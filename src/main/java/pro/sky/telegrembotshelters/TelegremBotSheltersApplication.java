51981ee657f229d8bf55c296e05ffda03baf9237
Smokvin <alex.smokvin@gmail.com>
2023-11-06 18:04:02 +0300

    initial commit

---
+++ src/main/java/pro/sky/telegrembotshelters/TelegremBotSheltersApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.telegrembotshelters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegremBotSheltersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegremBotSheltersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/telegrembotshelters/TelegremBotSheltersApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.telegrembotshelters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegremBotSheltersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
